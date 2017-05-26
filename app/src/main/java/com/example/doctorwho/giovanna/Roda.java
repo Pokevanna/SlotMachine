1c96c16feb60d8f9e47dc8d71c4f0382647468e7
logonrm <logonrm@fiap.com.br>
2017-05-26 08:46:43 -0300

    NAC - Inclusao no git

---
+++ app/src/main/java/com/example/doctorwho/giovanna/Roda.java
@@ -0,0 +1,58 @@
+package com.example.johw.giovanna;
+
+public class Roda extends Thread {
+
+    interface RodaListener{
+        void novaImagem(int resourceID);
+    }
+
+    private static int[] imagensSlots = {R.drawable.militariessanfrontiers_logo, R.drawable.metalgear_logo, R.drawable.foxhound_logo, R.drawable.diamonddogs_logo, R.drawable.metalgear_logo};
+    public int indiceAtual;
+    private RodaListener rodaListener;
+    private long duracaoDoQuadro;
+    private long iniciaEm;
+    private boolean isIniciado;
+
+    public Roda(RodaListener rodaListener, long duracaoDoQuadro, long iniciaEm){
+        this.rodaListener = rodaListener;
+        this.duracaoDoQuadro = duracaoDoQuadro;
+        this.iniciaEm = iniciaEm;
+        indiceAtual = 0;
+        isIniciado = true;
+    }
+
+    public void proximaImagem(){
+        indiceAtual++;
+
+        if(indiceAtual == imagensSlots.length){
+            indiceAtual = 0;
+        }
+    }
+
+    @Override
+    public void run(){
+        try{
+            Thread.sleep(iniciaEm);
+        } catch (InterruptedException e){
+
+        }
+
+        while(isIniciado){
+            try {
+                Thread.sleep(duracaoDoQuadro);
+            } catch (InterruptedException e){
+
+            }
+
+            proximaImagem();
+
+            if(rodaListener != null){
+                rodaListener.novaImagem(imagensSlots[indiceAtual]);
+            }
+        }
+    }
+
+    public void paraDeRodar(){
+        isIniciado = false;
+    }
+}
